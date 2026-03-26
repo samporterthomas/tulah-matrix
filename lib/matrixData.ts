8533e54bb2d99e4b82876054c4ee0e298c80b872
samporterthomas <75754556+samporterthomas@users.noreply.github.com>
2026-03-26 09:58:29 +0000

    Patch 5

--- lib/matrixData.ts
+++ lib/matrixData.ts
@@ -1,6 +1,4 @@
-// Matrix data loaded via JSON import (bundled at build time)
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const matrixRecords = require("../../public/matrix-data.json");
+import matrixRecords from "./matrix-data.json";
 
 export function getMatrixJson(): string {
   return JSON.stringify(matrixRecords);
